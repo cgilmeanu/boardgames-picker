dd80e90db16e5ac266ddf9e7f4cf0179c5297eab
Claudiu Gilmeanu <cgilmeanu@gmail.com>
2021-05-01 21:53:13 +0300

    initial commit

---
+++ apps/boardgames/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'boardgames-picker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'boardgames';
+}
